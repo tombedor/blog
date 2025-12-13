904509c1db7d368340963b52e33c8867610b6666
Tom Bedor <tombedor@gmail.com>
2025-12-13 11:16:49 -0800

    wip

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -91,6 +91,11 @@ const config: Config = {
           label: 'GitHub',
           position: 'right',
         },
+        {
+          href: '/rss.xml',
+          label: 'RSS',
+          position: 'right',
+        },
       ],
     },
     footer: {
