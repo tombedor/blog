5309d45e0b8d079a0f7ceaebf24f787c66673c73
Tom Bedor <tombedor@gmail.com>
2026-03-11 12:56:48 -0700

    wip

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,17 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export default function Tweet({ html }: { html: string }) {
+  const ref = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
+    const load = () => (window as any).twttr?.widgets?.load(ref.current);
+
     if ((window as any).twttr?.widgets) {
-      (window as any).twttr.widgets.load();
+      load();
     } else {
       const script = document.createElement('script');
       script.src = 'https://platform.twitter.com/widgets.js';
       script.async = true;
       script.charset = 'utf-8';
+      script.onload = load;
       document.body.appendChild(script);
     }
   }, []);
 
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+  return <div ref={ref} dangerouslySetInnerHTML={{ __html: html }} />;
 }
